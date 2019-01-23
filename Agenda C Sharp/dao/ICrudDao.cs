01d99ecb8efb61fab2ba1c5a8bcefb531d749498
JoaoPresto <jguedespresto@gmail.com>
2019-01-23 13:16:40 -0200

    Criação Interface CRUD
    Interface genérica do CRUD

---
+++ Agenda C Sharp/dao/ICrudDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda_C_Sharp.dao {
+    public interface ICrudDao<T> {
+
+        void Inserir(T entidade);
+        void Alterar(T entidade);
+        void Excluir(T endidade);
+        T retornaPorId(int id);
+        IList<T> Consultar();
+
+    }
+}
