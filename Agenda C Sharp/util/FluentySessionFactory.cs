8d0df0c6cee5b4459659333d86f79a9da495183b
JoaoPresto <jguedespresto@gmail.com>
2019-01-23 13:25:36 -0200

    Criação da classe FluentySessionFactory
    Classe que realiza conecção com o Banco

---
+++ Agenda C Sharp/util/FluentySessionFactory.cs
@@ -0,0 +1,29 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace Agenda_C_Sharp.util {
+    public class FluentySessionFactory {
+
+        private static string ConnectionString = "Server=localhost; Port=5432; User Id=postgres;" +
+                                                    "Password=1234; Database=agenda";
+        private static ISessionFactory session;
+
+        public static ISessionFactory CriarSession() {
+            if (session != null) {
+                return session;
+            }
+
+            IPersistenceConfigurer configBD = PostgreSQLConfiguration.PostgreSQL82.ConnectionString(ConnectionString);
+            var configMap = Fluently.Configure().Database(configBD).Mappings(c => c.FluentMappings.AddFromAssemblyOf<Mapeamento.ContatosMap>());
+            session = configMap.BuildSessionFactory();
+
+            return session;
+        }
+
+        public static ISession AbrirSession() {
+            return CriarSession().OpenSession();
+        }
+
+    }
+}
