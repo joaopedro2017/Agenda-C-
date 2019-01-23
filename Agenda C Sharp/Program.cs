761f4be78f83a0c5d79fa143e4f190311be9ec9c
JoaoPresto <jguedespresto@gmail.com>
2019-01-23 13:31:24 -0200

    Criação da Classe RepositoryCrudDao
    Classe com as ações CRUD conectando ao banco postgreSQL

--- Agenda C Sharp/Program.cs
+++ Agenda C Sharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Agenda_C_Sharp.GUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
---
+++ Agenda C Sharp/dao/RepositoryCrudDao.cs
@@ -0,0 +1,65 @@
+﻿using Agenda_C_Sharp.util;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Agenda_C_Sharp.dao {
+    public class RepositoryCrudDao<T> : ICrudDao<T> where T : class {
+        public void Alterar(T entidade) {
+            ISession session = FluentySessionFactory.AbrirSession();
+            ITransaction transaction = session.BeginTransaction();
+
+            try {
+                session.Update(entidade);
+                transaction.Commit();
+            } catch (Exception e) {
+                if (!transaction.WasCommitted) {
+                    transaction.Rollback();
+                }
+                throw new Exception("Erro ao alterar entidade: " + e.Message);
+            }
+        }
+
+        public IList<T> Consultar() {
+            ISession session = FluentySessionFactory.AbrirSession();
+            return (from e in session.Query<T>() select e).ToList();
+        }
+
+        public void Excluir(T entidade) {
+            ISession session = FluentySessionFactory.AbrirSession();
+            ITransaction transaction = session.BeginTransaction();
+
+            try {
+                session.Delete(entidade);
+                transaction.Commit();
+            } catch (Exception e) {
+                if (!transaction.WasCommitted) {
+                    transaction.Rollback();
+                }
+                throw new Exception("Erro ao excluir entidade: " + e.Message);
+            }
+        }
+
+        public void Inserir(T entidade) {
+            ISession session = FluentySessionFactory.AbrirSession();
+            ITransaction transaction = session.BeginTransaction();
+
+            try {
+                session.Save(entidade);
+                transaction.Commit();
+            } catch (Exception e) {
+                if (!transaction.WasCommitted) {
+                    transaction.Rollback();
+                }
+                throw new Exception("Erro ao inserir entidade: " + e.Message);
+            }
+
+        }
+
+        public T retornaPorId(int id) {
+            ISession session = FluentySessionFactory.AbrirSession();
+            return session.Get<T>(id);
+        }
+    }
+}
